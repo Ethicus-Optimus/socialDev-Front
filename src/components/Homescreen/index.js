cc52d5291ac161b90f6f2220b20df247a782bf30
mhendricks96 <mhendricks96@yahoo.com>
2022-03-15 11:55:45 -0700

    got Auth0 working along with switch between Landing Page and Home Screen

--- src/components/Homescreen/index.js
+++ src/components/Homescreen/index.js
@@ -3,11 +3,13 @@ import Footer from '../Footer'
 import Header from '../Header'
 import PostCard from '../PostCard'
 import NewNoteForm from '../NewNoteForm';
+import LogoutButton from '../LogoutButton';
 
 function Homescreen() {
   return (
     <div className="App">
       <Header />
+      <LogoutButton />
       <PostCard />
       <PostCard />
       <PostCard />
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import Footer from '../Footer'
 // import Header from '../Header'
 import LandingPage from '../LandingPage'
 import Homescreen from '../Homescreen'
+// import Login from '../Login'
+import { useAuth0 } from '@auth0/auth0-react'
+
 
 function Main() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading){
+    return <div>Loading...</div>;
+  }
+
+  // console.log(user);
+  // console.log(user.name);
+  // console.log(user.email);
+
+  // let email = user.email;
+  // console.log(email);
   return (
     <div className="App">
       {/* These 2 will be in a boolean that basically says - If signed in, show Homescreen, if not signed in, show LandingPage */}
-      {/* <LandingPage /> */}
-      <Homescreen />
+      {isAuthenticated ? <Homescreen /> : <LandingPage />}
+      {/* <LandingPage />
+      <Homescreen /> */}
     </div>
   )
 }
