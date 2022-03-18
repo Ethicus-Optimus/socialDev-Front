c2e2a844b0f8049b2faf66a073688d7fcdc59cc8
Dario Vitorte <vitortedario@gmail.com>
2022-03-18 10:41:18 -0700

    fr

--- src/components/PostCards/index.js
+++ src/components/PostCards/index.js
@@ -17,25 +17,25 @@ function PostCards({getUserPosts, user_posts, deletePost}) {
   const [show, setShow] = useState([false]);
   const [currentId, setCurrentId] = useState("")
 
-  const openModal = () => {
-    console.log('hi from open modal function')
-    setShow([true]);
-  }
+  // const openModal = () => {
+  //   console.log('hi from open modal function')
+  //   setShow([true]);
+  // }
   // const closeModal = () => setShow([false]);
   const setId = (id) => setCurrentId(id)
 
   // const addId = (id) => setShow(show.push(id))
 
-  const handleEditClick = (postId) => {
-    // addId(postId)
-    // console.log(postId)
-    console.log(show)
-    setId(postId)
-    console.log(currentId)
+  // const handleEditClick = (postId) => {
+  //   // addId(postId)
+  //   // console.log(postId)
+  //   console.log(show)
+  //   setId(postId)
+  //   console.log(currentId)
 
-    // openModal();
-    // editPost(postId)
-  }
+  //   // openModal();
+  //   // editPost(postId)
+  // }
 
 
   return (
